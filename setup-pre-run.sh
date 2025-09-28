2d80df43f1f8a84dcf71a7f94d0429b1e2edf770
Matteo Bertamini <dev@bertamini.net>
2025-09-28 03:39:51 +0200

    improve setup WIP to be done: 1. user and password for http auth are encoded automatically 2. .muttrc created at entrypoint via .env values

---
+++ setup-pre-run.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo "1. setting up .env"
+./setup-env.sh
+
+echo "2. setting up muttrc"
+./setup-muttrc.sh
+
---
+++ setup-run.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+docker compose exec -ti wallakindle /home/ubuntu/setup-wallakindle.sh	
